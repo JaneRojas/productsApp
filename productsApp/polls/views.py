3f620375b84939f732c1411c8b14d057c825390f
JeannetteRojas <janerrodriguez92@gmail.com>
2019-05-02 13:49:45 -0600

    adding views

--- productsApp/polls/views.py
+++ productsApp/polls/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import Product
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,13 @@ def index(request):
 def createProduct(request, product_id):
     return HttpResponse("You're creating a product %s." % product_id)
 
+def productLastExample(request, product_id):
+    try:
+        product = Product.objects.get(pk=product_id)
+    except Product.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/createProduct.html', {'product': product})
+
+def product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'polls/createProduct.html', {'product': product})
